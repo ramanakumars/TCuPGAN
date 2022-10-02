71289181811e9eeea44d7b635e775b610460973c
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2022-10-01 22:26:39 -0500

    modified unet training script for new changes

--- train_unet.py
+++ train_unet.py
@@ -10,17 +10,19 @@ from torch.optim.lr_scheduler import ExponentialLR
 
 device = 'cuda'
 
-base_folder = '/home/fortson/manth145/data/BraTS2020/Processed/Unaugmented/Training/'
-train_data, val_data = create_generators(base_folder + 'flair/', 
-                                        base_folder + 'seg/', batch_size=8)
+base_folder = '/home/fortson/rsankar/BraTS2020/training/'
+train_data, val_data = create_generators(base_folder,
+                                         batch_size=12)
 
 
-hidden        = [8, 16, 32, 64, 128]
-generator = LSTMUNet(hidden_dims=hidden).to(device)
+hidden = [6, 12, 18, 64, 128]
+bottleneck_dims = [64, 32, 16, 8, 6]
+generator = LSTMUNet(hidden_dims=hidden, bottleneck_dims=bottleneck_dims,
+                     input_channels=4).to(device)
 
-discriminator = PatchDiscriminator(input_channels=5, nfilt=8) 
+discriminator = PatchDiscriminator(input_channels=8, nfilt=16).to(device)
 
-summary(discriminator, input_size=(1, 155, 5, 128, 128), device=device)
+summary(generator, input_size=(1, 155, 4, 128, 128), device=device)
 
 
 trainer = Trainer(generator, discriminator, 'checkpoints-gamma05/')
@@ -28,4 +30,9 @@ trainer.fc_beta = 0.7
 
 trainer.load_last_checkpoint()
 
-trainer.train(train_data, val_data, 150, lr_decay=0.95)
+gen_learning_rate = 1.e-3*(0.95)**(trainer.start/5)
+dsc_learning_rate = 1.e-4*(0.95)**(trainer.start/5)
+
+trainer.train(train_data, val_data, 150, lr_decay=0.95, 
+              dsc_learning_rate=dsc_learning_rate,
+              gen_learning_rate=gen_learning_rate)
