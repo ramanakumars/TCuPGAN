84d3b787b6de0504621d3cb64c3e76d842f94a76
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2022-10-03 16:07:20 -0500

    added npy generator for MaNGA data

--- tcupgan/io.py
+++ tcupgan/io.py
@@ -3,12 +3,15 @@ import glob
 
 
 class DataGenerator:
+    file_type = 'npz'
+
     def __init__(self, datafolder,
                  batch_size, indices=None):
         self.img_datafolder = datafolder
         self.batch_size = batch_size
 
-        self.imgfiles = np.asarray(sorted(glob.glob(datafolder + "*.npz")))
+        self.imgfiles = np.asarray(
+            sorted(glob.glob(datafolder + f"*.{self.file_type}")))
 
         if indices is None:
             self.indices = np.arange(len(self.imgfiles))
@@ -18,9 +21,16 @@ class DataGenerator:
         self.ndata = len(self.indices)
 
         # get info about the data
-        img0 = np.load(self.imgfiles[0])['img']
 
-        self.d, _, self.h, self.w = img0.shape
+        if self.file_type == '.npz':
+            img0 = np.load(self.imgfiles[0])['img']
+        else:
+            img0 = np.load(self.imgfiles[0])
+
+        if len(img0.shape) == 3:
+            self.d, self.h, self.w = img0.shape
+        else:
+            self.d, _, self.h, self.w = img0.shape
 
         print(f"Found {self.ndata} images of shape {self.w}x{self.h}x{self.d}")
 
@@ -48,9 +58,29 @@ class DataGenerator:
         return imgs, segs
 
 
+class NpyDataGenerator(DataGenerator):
+    file_type = 'npy'
+
+    def __getitem__(self, index):
+        batch_indices = self.indices[index * self.batch_size:
+                                     (index + 1) * self.batch_size]
+
+        imgfiles = self.imgfiles[batch_indices]
+
+        imgs = np.zeros((self.batch_size, self.d, 1, self.h, self.w))
+
+        for i in range(self.batch_size):
+            imgs[i, :, 0, :, :] = np.load(imgfiles[i])
+
+        return imgs, None
+
+
 def create_generators(img_datafolder, batch_size,
-                      val_split=0.1):
-    imgfiles = np.asarray(sorted(glob.glob(img_datafolder + "*.npz")))
+                      val_split=0.1, datatype='npz'):
+    if datatype == 'npz':
+        imgfiles = np.asarray(sorted(glob.glob(img_datafolder + "*.npz")))
+    elif datatype == 'npy':
+        imgfiles = np.asarray(sorted(glob.glob(img_datafolder + "*.npy")))
 
     ndata = len(imgfiles)
 
@@ -63,9 +93,15 @@ def create_generators(img_datafolder, batch_size,
     val_ind = inds[:val_split_ind]
     training_ind = inds[val_split_ind:]
 
-    train_data = DataGenerator(img_datafolder, batch_size,
-                               indices=training_ind)
-    val_data = DataGenerator(img_datafolder, batch_size,
-                             indices=val_ind)
+    if datatype == 'npz':
+        train_data = DataGenerator(img_datafolder, batch_size,
+                                   indices=training_ind)
+        val_data = DataGenerator(img_datafolder, batch_size,
+                                 indices=val_ind)
+    elif datatype == 'npy':
+        train_data = NpyDataGenerator(img_datafolder, batch_size,
+                                      indices=training_ind)
+        val_data = NpyDataGenerator(img_datafolder, batch_size,
+                                    indices=val_ind)
 
     return train_data, val_data
