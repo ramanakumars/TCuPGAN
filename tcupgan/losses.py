db42d206d7b41054200bdde38cecf44d11d07fcb
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2022-10-06 22:12:08 -0500

    modified fc_tversky to sum along time axis. added MAE loss

--- tcupgan/losses.py
+++ tcupgan/losses.py
@@ -20,29 +20,33 @@ def tversky(y_true, y_pred, beta, batch_mean=True):
 
 def fc_tversky(y_true, y_pred, beta, gamma=0.5, batch_mean=True):
     smooth = 1
-    tp = torch.sum(y_true * y_pred, axis=(1, 2, 3, 4))
-    fn = torch.sum((1. - y_pred) * y_true, axis=(1, 2, 3, 4))
-    fp = torch.sum(y_pred * (1. - y_true), axis=(1, 2, 3, 4))
+    tp = torch.sum(y_true * y_pred, axis=(2, 3, 4))
+    fn = torch.sum((1. - y_pred) * y_true, axis=(2, 3, 4))
+    fp = torch.sum(y_pred * (1. - y_true), axis=(2, 3, 4))
     # tversky = reduce_mean(tp)/(reduce_mean(tp) +
     #                           beta*reduce_mean(fn) +
     #                           (1. - beta)*reduce_mean(fp))
     tversky = (tp + smooth) /\
         (tp + beta * fn + (1. - beta) * fp + smooth)
 
-    focal_tversky_loss = 1 - tversky
+    focal_tversky_loss = torch.sum(torch.pow(1 - tversky, gamma), axis = 1)
 
     if batch_mean:
-        return torch.pow(torch.mean(focal_tversky_loss), gamma)
+        return torch.mean(focal_tversky_loss)
     else:
-        return torch.pow(focal_tversky_loss, gamma)
+        return focal_tversky_loss
 
 
 def kl_loss(mu, sig):
     kl = 0.5*torch.mean(-1 - sig + torch.square(mu) + torch.exp(sig), axis=-1)
     return torch.mean(kl)
 
+def MAE(input_img, target_img):
+    diff = torch.mean(torch.abs(input_img - target_img), axis=(2,3,4))
+
+    return torch.mean(torch.sum(diff, axis=1))
 
 # alias
 generator_seg_loss = fc_tversky
-generator_vae_loss = nn.L1Loss()
+generator_vae_loss = MAE
 discriminator_loss = nn.BCELoss()
