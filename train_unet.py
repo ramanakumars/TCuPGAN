a390319efa38cdfcf1484f2addb6153c561d3213
Ramanakumar Sankar <rsankar@umn.edu>
2023-06-28 17:51:48 -0500

    adding a training script for BraTS

--- train_unet.py
+++ train_unet.py
@@ -2,21 +2,20 @@ from tcupgan import LSTMUNet, PatchDiscriminator, create_generators
 from tcupgan.trainer import Trainer
 from torchinfo import summary
 
-
 device = 'cuda'
 
 base_folder = '/home/fortson/rsankar/data/BraTS2021/training.resized/'
 train_data, val_data = create_generators(base_folder,
                                          batch_size=12)
 
-
 hidden = [6, 12, 18, 64, 128]
-bottleneck_dims = [64, 32, 16, 8, 6]
-generator = LSTMUNet(hidden_dims=hidden, bottleneck_dims=bottleneck_dims,
-                     input_channels=4).to(device)
+generator = LSTMUNet(hidden_dims=hidden,
+                     input_channels=4,
+                     output_channels=4).to(device)
 
 discriminator = PatchDiscriminator(input_channels=8, nlayers=2, nfilt=16).to(device)
 
+summary(generator, input_size=(1, 155, 4, 128, 128), device=device)
 summary(discriminator, input_size=(1, 155, 8, 128, 128), device=device)
 
 
@@ -25,6 +24,9 @@ trainer = Trainer(generator, discriminator, 'checkpoints.resized-gamma05/')
 
 gen_learning_rate = 1.e-3
 dsc_learning_rate = 1.e-4
+
+trainer.seg_alpha = 100
+trainer.loss_type = 'tversky'
 
 trainer.train(train_data, val_data, 150, lr_decay=0.95,
               dsc_learning_rate=dsc_learning_rate,
