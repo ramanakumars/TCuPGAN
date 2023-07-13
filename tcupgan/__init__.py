ef05b9a3ebddd1979f73232207808208a12b212b
Ramanakumar Sankar <rsankar@umn.edu>
2023-07-13 16:20:33 -0500

    fixing broken import

--- tcupgan/__init__.py
+++ tcupgan/__init__.py
@@ -1,7 +1,5 @@
 from .model import LSTMUNet
 from .vae import LSTMVAE
 from .disc import PatchDiscriminator
-from .io import DataGenerator, create_generators
 
-__all__ = ['LSTMVAE', 'LSTMUNet', 'PatchDiscriminator',
-           'DataGenerator', 'create_generators']
+__all__ = ['LSTMVAE', 'LSTMUNet', 'PatchDiscriminator']
